b9ed2e6078ced3412a20587da318f891ff78c896
Prathamesh <prathameshdhake30@gmail.com>
2025-12-18 15:33:50 -0500

    Add timed and untimed mode to the test.

--- speedtype.c
+++ speedtype.c
@@ -41,6 +41,10 @@ char* read_file(const char* filename) {
     fseek(fp, 0, SEEK_SET);
 
     char* content = malloc(size + 1);
+    if(!content){
+        fclose(fp);
+        return NULL;
+    }
     fread(content, 1, size, fp);
     content[size] = '\0';
     fclose(fp);
@@ -51,10 +55,10 @@ char* read_file(const char* filename) {
 void draw_text(WINDOW* win, const char* text, const char* typed, int pos, int* corr, int* incorr)
 {
     int len = strlen(text);
-    int y,x;
-    getyx(win, y, x);
-    int start_y = y;
-    int start_x = x;
+    // int y,x;
+    // getyx(win, y, x);
+    // int start_y = y;
+    // int start_x = x;
 
     *corr = 0;
     *incorr = 0;
@@ -106,13 +110,17 @@ int main(int argc, char *argv[])
     int pos = 0;
     int correct = 0;
     int incorrect = 0;
-    struct timespec start, finish, delta;
+    bool timed_mode = false;
+    double time_limit = 60.000;
+    struct timespec start, finish, delta, now;
 
 
     // Initialize ncurses
     initscr();
     //Check if Terminal supports color
     if (has_colors() == FALSE) {
+        free(text);
+        free(typed);
         endwin();
         printf("Your terminal does not support color\n");
         exit(1);
@@ -140,32 +148,92 @@ int main(int argc, char *argv[])
     curs_set(0);
     refresh();
     int ch = getch();
-    if( ch == 27){ //ESC KEY
-        endwin();
-        exit(1);
-    }
-    else if (ch == '\n' || ch == '\r' || ch == KEY_ENTER) // Enter
+    if(ch == '\n' || ch == '\r' || ch == KEY_ENTER) // Enter
     {
-        // Draw initial text
-        mvprintw(4,0, "Right, Starting in a second...\n");
+        // Timed Mode
+        mvprintw(4,0, "Timed Mode or No? Y/N\nDefault is non-timed");
         refresh();
-        clear();
-        draw_text(stdscr, text, typed, pos, &correct, &incorrect);
-        move(0,0);
-        refresh();
-        getch();
+        int chres = getch();
+        if (chres == 'Y' || chres == 'y'){
+            timed_mode = true;
+            refresh();}
 
-        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+            if(timed_mode){
+                clear();
+                // Draw initial text timed mode.
+                mvprintw(0,0, "Enter the number of seconds you wanna test for\nEnter a number between 0 and 600\nDefault is 60s");
+                refresh();
+                echo();        // show typed characters
+                curs_set(1);
+                char buf[8] = {0};
+                mvgetnstr(3, 0, buf, sizeof(buf)-1);
+
+                noecho();      // go back to hidden typing
+                curs_set(0);
+
+                if (buf[0] != '\0') {
+                    double val = atof(buf);
+                    if (val > 0.0 && val <= 600.0) {
+                        time_limit = val;
+                    }
+                }
+
+                clear();
+                mvprintw(0, 0, "Timed test: %6.3f seconds", time_limit);
+                move(2,0);
+                refresh();
+                draw_text(stdscr, text, typed, pos, &correct, &incorrect);
+            }
+            else{
+                refresh();
+                // Draw initial text Non timed mode.
+                mvprintw(1,0, "Right, Starting in a second...\n");
+                refresh();
+                clear();
+                draw_text(stdscr, text, typed, pos, &correct, &incorrect);
+                move(0,0);
+                refresh();
+            }
     }
     else{
-        mvprintw(4,0, "Wrong Key! Exiting in a second...\n");
-        refresh();
-        sleep(1);
+        free(text);
+        free(typed);
         endwin();
         exit(1);
     }
+    // ***********************TYPING TEST BEGINS*********************************
+    timeout(50); //to have a proper timer without waiting for user input
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     while (pos < len) {
+        if(timed_mode){
+            clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+            sub_timespec(start, now, &delta);
+            double elapsed = delta.tv_sec + delta.tv_nsec / 1e9;
+            double time_left = time_limit - elapsed;
+            if(time_left < 0){
+                time_left = 0;
+            }
+            if(time_left == 0)
+            {
+                break;
+            }
+            char timer_str[32];
+            snprintf(timer_str, sizeof(timer_str), "Time: %6.3fs", time_left);
+
+            int x = (COLS - (int)strlen(timer_str)) / 2;
+            if (x < 0) x = 0;
+
+            move(0, 0);
+            clrtoeol();             // clear the whole top line
+            attron(COLOR_PAIR(PAIR_GREEN));
+            mvprintw(0, x, "%s", timer_str);
+            attroff(COLOR_PAIR(PAIR_GREEN));
+        }
         ch = getch();
+
+        if(ch == ERR){
+            continue;
+        }
         if (ch == 27) {  // ESC key
             break;
         } else if (ch == KEY_BACKSPACE || ch == 127 || ch == '\b') {
@@ -180,7 +248,8 @@ int main(int argc, char *argv[])
         }
 
         // Redraw text
-        move(0, 0);
+        if (timed_mode) move(2, 0);
+        else move(0,0);
         clrtoeol();
         draw_text(stdscr, text, typed, pos, &correct, &incorrect);
     }
